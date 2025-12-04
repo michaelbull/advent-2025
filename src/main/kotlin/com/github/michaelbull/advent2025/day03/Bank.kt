bf69c27d121d941886c18ac5d4e3374cabdbf73a
Michael Bull <mikebull94@gmail.com>
2025-12-04 19:58:18 +0000

    Convert day 3 solution to tail-recursive function

--- src/main/kotlin/com/github/michaelbull/advent2025/day03/Bank.kt
+++ src/main/kotlin/com/github/michaelbull/advent2025/day03/Bank.kt
@@ -1,25 +1,17 @@
 package com.github.michaelbull.advent2025.day03
 
 @JvmInline
-value class Bank(private val string: String) {
-    fun maxJoltage(digits: Int): Long {
-        val digitRange = 0 until digits
-
-        val (_, joltage) = digitRange.fold(0 to "") { (start, joltage), selected ->
-            val remaining = digits - selected
-            val endInclusive = string.length - remaining
+value class Bank(private val value: String) {
+    tailrec fun maxJoltage(digits: Int, start: Int = 0, joltage: Long = 0L): Long {
+        return if (digits == 0) {
+            joltage
+        } else {
+            val endInclusive = value.length - digits
             val range = start..endInclusive
-
-            val maxIndex = range.maxBy(string::get)
-            val nextIndex = maxIndex + 1
-
-            val digit = string[maxIndex]
-            val nextJoltage = joltage + digit
-
-            nextIndex to nextJoltage
+            val maxIndex = range.maxBy(value::get)
+            val digit = value[maxIndex].digitToInt()
+            maxJoltage(digits - 1, maxIndex + 1, (joltage * 10) + digit)
         }
-
-        return joltage.toLong()
     }
 }
 
